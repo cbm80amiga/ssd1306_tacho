60d2af32777908f0c874ae46cfa604c6fd8f3d31
cbm80amiga <pawelhernik123@gmail.com>
2021-12-15 07:31:47 +0100

    Add files via upload

---
+++ OLED_SoftI2C.h
@@ -0,0 +1,275 @@
+// Fast OLED SoftI2C/HW I2C
+// 2015-2016 Pawel A. Hernik
+
+#ifndef _OLED_SoftI2C_H
+#define _OLED_SoftI2C_H 
+
+#define OLED_CMD 0x00   // Co = 0, D/C = 0
+#define OLED_DAT 0x40   // Co = 0, D/C = 1
+
+#include <Arduino.h>
+#include <avr/pgmspace.h>
+
+// ---------------------------------
+class OLEDSoftI2C {
+ public:
+// ---------------------------------
+OLEDSoftI2C()
+{
+  i2cAddr = 0x3c;
+}
+// ---------------------------------
+OLEDSoftI2C(int addr)
+{
+  i2cAddr = addr;
+}
+// ---------------------------------
+void setFont(const uint8_t* _font)
+{
+  font     = _font;
+  xSize    =-pgm_read_byte(font+0);
+  ySize    = pgm_read_byte(font+1);
+  offs     = pgm_read_byte(font+2);
+  numChars = pgm_read_byte(font+3);
+  ySize8   = (ySize+7)/8;
+  minCharWd = 0;
+}
+// ---------------------------------
+void setMinCharWd(uint8_t wd) { minCharWd = wd; }
+// ---------------------------------
+void printStr(uint8_t x, uint8_t row, char *characters)
+{
+  uint8_t xpos = x;
+  uint8_t ypos = row;
+  while (*characters) {
+    if(*characters==10) { // \n clears area to the end of line and skips to the next line
+      fillWin(xpos,ypos,128-xpos,ySize8,0);
+      xpos = 0;
+      ypos += ySize8;
+      characters++;
+    } else {
+      int wd = printChar(xpos, ypos, *characters++);
+      xpos += wd;
+      if(xpos>127) {
+        xpos = 0;
+        ypos += ySize8;
+      }
+    }
+  }
+}
+// ---------------------------------
+uint8_t printChar(uint8_t x, uint8_t row, uint8_t ch)
+{
+  if(ch < offs)
+    return 0;
+
+  int i, idx = 4 + (ch - offs)*(xSize*ySize8+1);
+  int wd = pgm_read_byte(font + idx++);
+  int wdL = 0, wdR = 1; // default spacing before and behind char
+  if(minCharWd>wd) {
+    wdL = (minCharWd-wd)/2;
+    wdR += minCharWd-wd-wdL;
+  }
+  setVWin(x, row, wd+wdL+wdR, ySize8);
+  wdL *= ySize8;
+  wdR *= ySize8;
+  wd *= ySize8;
+#if USEHW==1
+  Wire.beginTransmission(i2cAddr);
+  Wire.write(OLED_DAT);
+  for(i=0; i<wdL; i++) Wire.write(0);
+  for(i=0; i<wd; i++)  Wire.write(pgm_read_byte(font+idx+i));
+  for(i=0; i<wdR; i++) Wire.write(0);
+  Wire.endTransmission();
+#else
+  i2c_start(i2cAddr<<1 | I2C_WRITE);
+  i2c_write(OLED_DAT);
+  for(i=0; i<wdL; i++) i2c_write(0);
+  for(i=0; i<wd; i++)  i2c_write(pgm_read_byte(font+idx+i));
+  for(i=0; i<wdR; i++) i2c_write(0);
+  i2c_stop();
+#endif
+  return (wd+wdL+wdR)/ySize8;
+}
+// ---------------------------------
+void setHWin(uint8_t colS, uint8_t rowS, uint8_t wd, uint8_t ht)
+{
+  writeCmd(0x20); //set horizontal addressing mode for screen clear
+  writeCmd(0x00);
+  
+  writeCmd(0x21); //set column start and end address
+  writeCmd(colS); //set column start address
+  writeCmd(colS+wd-1); //set column end address
+  
+  writeCmd(0x22); //set row start and end address
+  writeCmd(rowS); //set row start address
+  writeCmd(rowS+ht-1); //set row end address
+}
+// ---------------------------------
+void setVWin(uint8_t colS, uint8_t rowS, uint8_t wd, uint8_t ht)
+{
+  writeCmd(0x20); //set horizontal addressing mode for screen clear
+  writeCmd(0x01);
+  
+  writeCmd(0x21); //set column start and end address
+  writeCmd(colS); //set column start address
+  writeCmd(colS+wd-1); //set column end address
+  
+  writeCmd(0x22); //set row start and end address
+  writeCmd(rowS); //set row start address
+  writeCmd(rowS+ht-1); //set row end address
+}
+// ---------------------------------
+void writeData(byte data)
+{
+#if USEHW==1
+  Wire.beginTransmission(i2cAddr);
+  Wire.write(OLED_DAT);
+  Wire.write(data);
+  Wire.endTransmission();
+#else
+  i2c_start(i2cAddr<<1 | I2C_WRITE);
+  i2c_write(OLED_DAT);
+  i2c_write(data);
+  i2c_stop();
+#endif
+}
+// ---------------------------------
+void writeCmd(byte command)
+{
+#if USEHW==1
+  Wire.beginTransmission(i2cAddr);
+  Wire.write(OLED_CMD);
+  Wire.write(command);
+  Wire.endTransmission();
+#else
+  i2c_start(i2cAddr<<1 | I2C_WRITE);
+  i2c_write(OLED_CMD);
+  i2c_write(command);
+  i2c_stop();
+#endif
+}
+// ---------------------------------
+void init()
+{
+#if USEHW==1
+  Wire.begin();
+  Wire.setClock(800000); // faster I2C
+#else
+  Serial.println(i2c_init() ? F("SoftI2C initialization done") : F("SoftI2C initialization error. SDA or SCL are low"));
+#endif
+  
+  writeCmd(0x8d); //enable charge pump
+  writeCmd(0x14);
+  delay(1);
+  writeCmd(0xaf); //set display on
+
+//  writeCmd(0xa8); //set MUX ratio
+//  writeCmd(0x3f);
+  writeCmd(0xd3); //set display offset
+  writeCmd(0x00);
+  writeCmd(0x40); //set display start line
+  writeCmd(0xa1); //set segment re-map (horizontal flip) - reset value is 0xa0 (or 0xa1)
+  writeCmd(0xc8); //set COM output scan direction (vertical flip) - reset value is 0xc0 (or 0xc8)
+  writeCmd(0xda); //set COM pins hardware configuration
+  writeCmd(0x12); //reset value is 0x12
+  writeCmd(0x81); //set contrast (2-byte)
+  writeCmd(0xff);
+//  writeCmd(0xa4); //disable entire display on
+//  writeCmd(0xa6); //set normal display
+//  writeCmd(0xd5); //set oscillator frequency
+//  writeCmd(0x80);
+//  writeCmd(0xdb); //vcomh deselect level (brightness)
+//  writeCmd(0x20);
+}
+// ---------------------------------
+void fillWin(int x, int y, int w, int h, int val)
+{
+  setVWin(x,y,w,h);
+#if USEHW==1
+  for (int i=0; i<1024; i+=16) 
+  {
+    Wire.beginTransmission(i2cAddr);
+    Wire.write(OLED_DAT);
+    for(int i=0; i<(128-x)*h; i++) Wire.write(val);
+    Wire.endTransmission();
+  }
+#else
+  i2c_start(i2cAddr<<1 | I2C_WRITE);
+  i2c_write(OLED_DAT);
+  for(int i=0; i<(128-x)*h; i++) i2c_write(val);
+  i2c_stop();
+#endif
+}
+// ---------------------------------
+void clrScr()
+{
+  setVWin(0,0,128,8);
+#if USEHW==1
+  for (int i=0; i<1024; i+=16) 
+  {
+    Wire.beginTransmission(i2cAddr);
+    Wire.write(OLED_DAT);
+    for(int j=0; j<4; j++) 
+      {Wire.write(0); Wire.write(0); Wire.write(0); Wire.write(0);}
+    Wire.endTransmission();
+  }
+#else
+  i2c_start(i2cAddr<<1 | I2C_WRITE);
+  i2c_write(OLED_DAT);
+  for (int i=0; i<1024/4; i++) {i2c_write(0); i2c_write(0); i2c_write(0); i2c_write(0); }
+  i2c_stop();
+#endif
+}
+/*
+void scroll()
+{
+  writeCmd(0x27); //set right horizontal scroll
+  writeCmd(0x0);  //dummy byte
+  writeCmd(0x0);  //page start address
+  writeCmd(0x7);  //scroll speed
+  writeCmd(0x7);  //page end address
+  writeCmd(0x0);  //dummy byte
+  writeCmd(0xff); //dummy byte
+  writeCmd(0x2f); //start scrolling
+}
+*/
+// ---------------------------------
+void drawBitmap(const uint8_t *bmp, uint8_t colS, uint8_t rowS, uint8_t wd, uint8_t ht)
+{
+  setHWin(colS,rowS,wd,ht);
+#if USEHW==1
+  for (int i=0; i<wd*ht; i++) {
+    Wire.beginTransmission(i2cAddr);
+    Wire.write(OLED_DAT);
+    Wire.write(pgm_read_byte(bmp+i));
+    Wire.endTransmission();
+  }
+#else
+  i2c_start(i2cAddr<<1 | I2C_WRITE);
+  i2c_write(OLED_DAT);
+  for (int i=0; i<wd*ht; i++) 
+     i2c_write(pgm_read_byte(bmp+i));
+  i2c_stop();
+#endif
+}
+// ---------------------------------
+void drawBitmap(const uint8_t *bmp, uint8_t colS, uint8_t rowS)
+{
+  uint8_t wd = pgm_read_byte(bmp+0);
+  uint8_t ht = pgm_read_byte(bmp+1);
+  drawBitmap(bmp+2, colS, rowS, wd, ht);
+}
+// ---------------------------------
+ private:
+  int i2cAddr = 0x3c;
+  const uint8_t* font; 
+  uint8_t xSize;
+  uint8_t ySize;
+  uint8_t ySize8;
+  uint8_t offs;
+  uint8_t numChars;
+  uint8_t minCharWd;
+};
+#endif
+
---
+++ ssd1306_tacho2.ino
@@ -0,0 +1,118 @@
+// Fidget Spinner counter and tachometer
+// 27.08.2017 Pawel A. Hernik
+// source code for the video:
+// https://youtu.be/42qNfPOYlR8
+
+/*
+ Parts:
+ - Hall sensor 3144
+ - Neodymium magnet for fidget spinner
+ - OLED SSD3106
+ - Arduino Pro Mini/Nano
+ 
+ 3144 pinout from front:
+ 1 - VCC
+ 2 - GND
+ 3 - DATA (0 when close to magnet)
+*/
+
+#define USEHW 0  // 0 - use fast softi2c, 1 - use hw implementation on A4/A5
+
+#if USEHW==1
+#include <Wire.h>
+#else
+//#define SDA_PORT PORTC
+//#define SDA_PIN 4  // A4
+//#define SCL_PORT PORTC
+//#define SCL_PIN 5  // A5
+
+#define SDA_PORT PORTB
+#define SDA_PIN 3  // D11
+#define SCL_PORT PORTB
+#define SCL_PIN 2  // D10
+
+#define I2C_FASTMODE 1
+#include <SoftI2CMaster.h>
+#endif
+
+#include "OLED_SoftI2C.h"
+#include "term8x14_font.h"
+
+const int hallPin = 2; // pin 2 = int 0
+
+OLEDSoftI2C oled(0x3c);
+char txt[10];
+
+// --------------------------------------------------------------------------
+
+volatile unsigned long cntTime=0;
+volatile unsigned long cnt=0;
+
+void doCount() // interrupt callback should be as short as possible!
+{
+  if(digitalRead(hallPin) == LOW)
+  {
+    cnt++;
+    cntTime = millis();
+  }
+}
+
+// --------------------------------------------------------------------------
+
+void setup()
+{
+  Serial.begin(9600);
+  oled.init();
+  oled.clrScr();
+  oled.setFont(Term8x14PL);
+  oled.printStr(0, 0, "Rot:");
+  oled.printStr(0, 2, "RPM:");
+  oled.printStr(0, 4, "Max:");
+  oled.printStr(0, 6, "Time:        s");
+  oled.setMinCharWd(8);
+  pinMode(hallPin,INPUT_PULLUP);
+  digitalWrite(hallPin,HIGH);
+  attachInterrupt(digitalPinToInterrupt(hallPin), doCount, FALLING);  // hall pin on interrupt 0 = pin 2
+  digitalWrite(hallPin,HIGH);
+  pinMode(hallPin,INPUT_PULLUP);
+  cntTime = millis();
+}
+
+// --------------------------------------------------------------------------
+
+volatile unsigned long rpm=0,maxrpm=0;
+int dispRotTime=0, rotTime=0;
+unsigned long measureTime=0,curTime,startTime=0;
+int dispCnt=0,measureCnt=0;
+
+const int resetTime = 2000;
+const int minRotNum = 1;  // 1 - calc after every rotation
+
+void loop()
+{
+  curTime = millis();
+  if(curTime-cntTime>resetTime) { // reset when less than 30RPM (dt>2s)
+    cnt = measureCnt = 0;
+    rpm = 0;
+  }
+  if(cnt==1) startTime = cntTime;
+  if(cnt-measureCnt>=minRotNum) {
+    rpm = 60000L*(cnt-measureCnt)/(cntTime-measureTime);
+    //Serial.println(String("Cnt=")+cnt+" dcnt="+(cnt-measureCnt)+" dtime="+(cntTime-measureTime));
+    measureCnt = cnt;
+    measureTime = cntTime;
+  }
+  rotTime = (cntTime-startTime)/1000; // time in seconds
+  if(cnt>1 || !dispRotTime) {  // keep previous time on the OLED until new rotation starts
+    dispRotTime = rotTime;
+    dispCnt = cnt;
+  }
+  if(rpm>maxrpm) maxrpm=rpm;
+  sprintf(txt,"% 5u",dispCnt);     oled.printStr(38, 0, txt);
+  sprintf(txt,"% 5u",rpm);         oled.printStr(38, 2, txt);
+  sprintf(txt,"% 5u",maxrpm);      oled.printStr(38, 4, txt);
+  sprintf(txt,"% 5u",dispRotTime); oled.printStr(38, 6, txt);
+}
+
+// --------------------------------------------------------------------------
+
